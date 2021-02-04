4d879bf28e92397a0fd53fb8566ff3f698feb2cd
Roberto Rosa <roberto.rosa7@gmail.com>
2021-02-04 16:05:27 -0300

    format code

--- random_chat/src/pages/app/app-component.js
+++ random_chat/src/pages/app/app-component.js
@@ -1,27 +1,29 @@
-import Format from '../../utils/format';
-import User from '../../services/user';
+import Format from "../../utils/format";
+import User from "../../services/user";
 
-import ProtoService from '../../services/prototype-serivce';
-import RoutesService from '../../services/routes-service';
-import Database from '../../services/websql-service';
+import ProtoService from "../../services/prototype-serivce";
+import RoutesService from "../../services/routes-service";
+import Database from "../../services/websql-service";
 
-export default class AppPage{
-  constructor(){
+export default class AppPage {
+  constructor() {
     new ProtoService();
     this.db = new Database();
     this.router = new RoutesService();
     this.el = {};
-    document.querySelectorAll('[id]').forEach(element => {
-      this.el[Format.formatToCamelCase(element.id)] = element;
-    });
+    document
+      .querySelectorAll("[id]")
+      .forEach(
+        (element) => (this.el[Format.formatToCamelCase(element.id)] = element)
+      );
     this.fetchUser();
   }
 
-  async fetchUser(){
-    const { email } = JSON.parse(localStorage.getItem('isLogged'));
-    const db = await this.db.createIndexdb('users');
+  async fetchUser() {
+    const { email } = JSON.parse(localStorage.getItem("isLogged"));
+    const db = await this.db.createIndexdb("users");
     const data = await this.db.databaseIsReady(db);
-    const user = await this.db.getData(data, 'users', email);
+    const user = await this.db.getData(data, "users", email);
 
     this.user = new User(email);
     this.user.name = user.name;
@@ -29,23 +31,33 @@ export default class AppPage{
     this.user.photo = user.photo;
 
     await this.user.save();
-      
-    try{
-      this.user.on('contactschange', docs => {
+
+    try {
+      this.user.on("contactschange", (docs) => {
         const contacts = [];
-        docs.forEach(doc => { contacts.push(doc.data()); });
-        document.querySelector('app-list-contacts')
-          .dispatchEvent(new CustomEvent('contactIsLoaded', {detail: contacts}));
+        docs.forEach((doc) => {
+          contacts.push(doc.data());
+        });
+        document
+          .querySelector("app-list-contacts")
+          .dispatchEvent(
+            new CustomEvent("contactIsLoaded", { detail: contacts })
+          );
       });
 
       await this.user.getContacts();
+    } catch (e) {
+      console.error(e);
+    }
 
-    }catch(e){ console.error(e) }
+    document.querySelector("title").innerHTML = this.user.name
+      ? this.user.name
+      : this.user.email + " Random chat";
+    this.el.app.dispatchEvent(new Event("isAuth"));
 
-    document.querySelector('title').innerHTML = (this.user.name) ? this.user.name : this.user.email + ' Random chat';
-    this.el.app.dispatchEvent(new Event('isAuth'));
-    
-    this.notification(`Bem vindo(a) ${(this.user.name) ? this.user.name : this.user.email}`);
+    this.notification(
+      `Bem vindo(a) ${this.user.name ? this.user.name : this.user.email}`
+    );
   }
 
   // closeBtnDialog(){
@@ -73,8 +85,9 @@ export default class AppPage{
   //         }
   //     });
   // }
-  notification(text){
-    document.querySelector('app-snackbar')
-      .dispatchEvent(new CustomEvent('show', {detail: text}));
+  notification(text) {
+    document
+      .querySelector("app-snackbar")
+      .dispatchEvent(new CustomEvent("show", { detail: text }));
   }
 }
--- random_chat/src/services/auth-service.js
+++ random_chat/src/services/auth-service.js
@@ -1,17 +1,25 @@
 import Firebase from "./firebase";
 
-export default class Auth{
-	constructor(){
-		this.fbService = new Firebase();
-	}
-	async initAuth(){
-		return new Promise(async (resolve, reject) => {
-			try{ resolve(await this.fbService.initAuth()) }catch(e){ reject(e) }
-		});
-	}
-	async initAuthWidthEmailPassword(payload){
-		return new Promise(async (resolve, reject) => {
-			try{ resolve(await this.fbService.initLoginWidthEmail(payload)) }catch(e){ reject(e) }
-		});
-	}
+export default class Auth {
+  constructor() {
+    this.fbService = new Firebase();
+  }
+  async initAuth() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        resolve(await this.fbService.initAuth());
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+  async initAuthWidthEmailPassword(payload) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        resolve(await this.fbService.initLoginWidthEmail(payload));
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
 }
