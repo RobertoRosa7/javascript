6ef1c2374373fad99ae047c167dcdd1653bface8
Roberto Rosa da Silva <44420577+RobertoRosa7@users.noreply.github.com>
2021-09-06 16:00:25 -0300

    update

---
+++ how_to/calendar/app.js
@@ -0,0 +1,4 @@
+document.querySelector('.dark-mode-switch').onclick = () => {
+  document.querySelector('body').classList.toggle('dark')
+  document.querySelector('body').classList.toggle('light')
+}
