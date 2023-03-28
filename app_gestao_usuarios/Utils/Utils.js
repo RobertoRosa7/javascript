52149b43b92959f39a1df92a479268c806a0bbaf
Roberto Rosa <roberto.rosa7@gmail.com>
2023-03-28 08:32:28 -0300

    update

--- app_gestao_usuarios/Utils/Utils.js
+++ app_gestao_usuarios/Utils/Utils.js
@@ -1,5 +1,15 @@
-class Utils{
-	static dateFormat(date){
-		return date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear()+' '+date.getHours()+':'+date.getMinutes();
-	}
+class Utils {
+  static dateFormat(date) {
+    return (
+      date.getDate() +
+      "/" +
+      (date.getMonth() + 1) +
+      "/" +
+      date.getFullYear() +
+      " " +
+      date.getHours() +
+      ":" +
+      date.getMinutes()
+    );
+  }
 }
